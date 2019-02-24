34af7877c6d6894661496eedbc20d806f96acfe3
Osama Adel <o.adel@devisionx.com>
2019-02-24 20:48:20 +0200

    Refactor linear_regression and logistic_regression to use classes

--- linear_regression.py
+++ linear_regression.py
@@ -1,25 +1,54 @@
-#%%
-# Linear Regression Using Python
-
+# Linear Regression
 #%%
 import numpy as np
 import random
 from matplotlib import pyplot as plt
 
 #%%
-# Let's assume we have a straight line:
-# y = 4 * x + 10
-# We need to create a dataset that approcimately
-# represents this line.
+class LinearRegression():
+    def __init__(self):
+        self.w = random.random()
+        self.b = random.random()
+    
+    def train(self, X, y, print_params = False):
+        """ X is M x 1 matrix and y is M x 1 matrix """
+        alpha = 0.001
+        thresh = 1.1
+        i = 1
+        m = y.shape[0]
+        while(True):
+            y_pred = self.w * X + self.b
+            J_train = np.sum((y_pred - y)**2) / m
+            dw = 2 * np.sum((y_pred - y) * X) / m
+            db = 2 * np.sum(y_pred - y) / m
+            self.w = self.w - alpha * dw
+            self.b = self.b - alpha * db
+            if print_params:
+                print("Step #{0}".format(i))
+                print("J_train:", J_train)
+                print("w:", self.w, "b:", self.b)
+                print()
+            i += 1
+            if J_train < thresh:
+                print("=> Training is complete.")
+                break
+
+    
+    def predict(self, x_test):
+        """ Returns the prediction for all x_test """
+        return self.w * x_test + self.b
+#%%
+# Generating random dataset
+# Let's assume we have a straight line: y = 4 * x + 10,
+# We need to create a dataset that approcimately represents this line.
 x = np.random.randn(1000, 1) + 4
 rand_bias = np.random.randn(1000, 1) 
-# print(x.shape)
-#%%
 y = 4 * x + 10 + rand_bias
-plt.figure(0)
-plt.plot(x, y, "r*")
-# plt.show()
+print("x is", x.shape)
+print("y is", y.shape)
+
 #%%
+# Splitting dataset into training and test sets
 x_train = x[:800,:]
 y_train = y[:800,:]
 x_test = x[800:,:]
@@ -29,41 +58,25 @@ print("y_train:", y_train.shape)
 print("x_test:", x_test.shape)
 print("y_test:", y_test.shape)
 #%%
+# Plot test set examples
 # plt.figure(1)
 # plt.plot(x_test, y_test, "r*")
 # plt.show()
 #%%
-# Now we need to calculate to built a model with
-# initialized parameters vector w
-w = random.random()
-b = random.random()
-alpha = 0.001
-thresh = 1.1
-print("w:", w, "b:", b)
-#%%
-i = 0
-while(True):
-    print("Step #{0}".format(i+1))
-    y_pred = w * x_train + b
-    J_train = np.sum((y_pred - y_train)**2) / 800
-    print("J_train:", J_train)
-    dw = 2 * np.sum((y_pred - y_train) * x_train) / 800
-    db = 2 * np.sum(y_pred - y_train) / 800
-    w = w - alpha * dw
-    b = b - alpha * db
-    print("w:", w, "b:", b)
-    print()
-    i += 1
-    if J_train < thresh:
-        break
+# Create and traing a linear regression model
+model = LinearRegression()
+model.train(x_train, y_train)
 
 #%%
-y_pred = w * x_test + b
-J_test = np.sum((y_pred - y_test)**2) / 200
+# Generating predictions for test set
+y_pred = model.predict(x_test)
+J_test = np.sum((y_pred - y_test)**2) / y_pred.shape[0]
 print("J_test:", J_test)
+
 #%%
-xline = np.array([[1], [6]])
-yline = w * xline + b
+# Plotting the model against the data examples
+xline = np.array([[np.min(x_train)], [np.max(x_train)]])
+yline = model.predict(xline)
 plt.figure(0)
 
 plt.plot(x, y, "r*")
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,51 +1,90 @@
 # Logistic regression
 #%%
+# Importing libraries
 import numpy as np
 import random
 from matplotlib import pyplot as plt
-#%%
-def sigmoid(z):
-    return 1 / (np.exp(-1 * z))
-#%%
-def step(w, b, X, y, alpha, m):
-    # print("w.shape:",w.shape)
-    # print("b.shape:", b.shape)
-    # print("X.shape:", X.shape)
-    # print("y.shape:", y.shape)
-    Z = np.dot(w, X.T) + b
-    # print("Z.shape:", Z.shape)
-    A = sigmoid(Z)
-    # print("A.shape:", A.shape)
-    dZ = A - y
-    # print("dZ.shape:", dZ.shape)
-    dw = np.dot(dZ, X) / m
-    db = np.sum(dZ) / m
-    w = w - alpha * dw
-    b = b - alpha * db
-    J = np.sum(-y * np.log(A) - (1-y) * np.log(1-A)) / m
-    return w, b, J
-#%%
-x = np.linspace(1, 10, 1000)[:, np.newaxis]
-y = np.hstack((np.zeros((1,500)), np.ones((1,500))))
-y[0,random.randint(0,500)] = 1
-y[0,random.randint(500,1000)] = 0
-print(y.shape)
 
 #%%
-w = np.random.rand(1,1)
-b = np.random.rand(1,1)
-alpha = 0.01
-m = 1000
-for i in range(10000):
-    w, b, J = step(w, b, x, y, alpha, m)
-    print("J:", J)
-    print("w:", w, "b:", b)
-    print()
+class LogisticRegression():
+    def __init__(self):
+        self.w = np.random.rand(1,1)
+        self.b = np.random.rand(1,1)
+        self.J = 0
+    
+    def __sigmoid(self, z):
+        return 1 / (1 + (np.exp(-1 * z)))
+    
+    def __step(self, w, b, X, y, alpha, m, print_params=False):
+        """ Implements one step of backpropagation """
+        if print_params:
+            print("w.shape:",w.shape)
+            print("b.shape:", b.shape)
+            print("X.shape:", X.shape)
+            print("y.shape:", y.shape)
+        Z = np.dot(w, X) + b
+        # print("Z.shape:", Z.shape)
+        A = self.__sigmoid(Z)
+        # print("A.shape:", A.shape)
+        dZ = A - y
+        # print("dZ.shape:", dZ.shape)
+        dw = np.dot(dZ, X.T) / m
+        db = np.sum(dZ) / m
+        w = w - alpha * dw
+        b = b - alpha * db
+        J = np.sum(-y * np.log(A) - (1-y) * np.log(1-A)) / m
+        return w, b, J
+    
+    def train(self, X, y, print_params=False):
+        """ Trains logsitic regression model - X is N x M matrix and y is 1 x M matrix """
+        alpha = 0.01
+        m = 1000
+        for i in range(100000):
+            self.w, self.b, self.J = self.__step(self.w, self.b, X, y, alpha, m)
+            if print_params:
+                print("J:", self.J)
+                print("w:", self.w, "b:", self.b)
+                print("========")
+        
+    def predict(self, x_tst):
+        """ Returns the prediction of the input x_tst - x_tst should be 1 x N vector """
+        return self.__sigmoid(np.dot(self.w, x_tst) + self.b)
+
 #%%
-x1 = np.array([[700]])
-y1 = sigmoid(w * x1 + b)
-print(y1)
+# Generating training data
+x = np.linspace(1, 10, 1000)[:, np.newaxis].T
+y = np.hstack((np.zeros((1,500)), np.ones((1,500))))
+
+# Adding some randomness
+y[0,random.randint(0,500)] = 1
+y[0,random.randint(500,1000)] = 0
+print("X is", x.shape)
+print("y is", y.shape)
+
 #%%
+# Creating and training the model
+model = LogisticRegression()
+model.train(x, y)
+
+#%%
+# Generating a test example
+x1 = np.array([[0.05]])
+# Predicting the output
+y1 = model.predict(x1)
+print("Prediction of", x1, " is", y1)
+
+#%%
+# Generating predictions for all input examples X
+x_test = np.linspace(1, 10, 100)[:,np.newaxis].T
+y_p = model.predict(x_test)
+
+#%%
+# Plotting training data
 plt.figure(0)
-plt.plot(x.T, y, "r*")
+plt.plot(x, y, "r+")
+# plt.show()
+# Plotting predictions
+print("x is", x.shape, "and y_p is", y_p.shape)
+print("max of y_p:", np.max(y_p))
+plt.plot(x_test, y_p, "bo")
 plt.show()
